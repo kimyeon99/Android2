c6abdba094f6f838965891687067d2ecc10e9439
kimyeon99 <tmdwo7017@naver.com>
2021-10-12 23:28:08 +0900

    2021-10-12 Study Android

---
+++ viewFlipper/app/src/main/java/com/yeungjin/viewflipper/MainActivity.java
@@ -0,0 +1,36 @@
+package com.yeungjin.viewflipper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnPrev, btnNext;
+    ViewFlipper vFilpper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnPrev = (Button) findViewById(R.id.btnPrev);
+        btnNext = (Button) findViewById(R.id.btnNext);
+        vFilpper = (ViewFlipper) findViewById(R.id.viewFlipper1);
+
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vFilpper.showPrevious();
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vFilpper.showNext();
+            }
+        });
+    }
+}
